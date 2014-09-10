cb4726b3eb23f12f98c9c73b3be25a516e1e1785
mpoyner <mpoyner@gmail.com>
2014-09-10 16:24:44 -0400

    carousel fixes

--- assets/js/initializeSlickCarousel.js
+++ assets/js/initializeSlickCarousel.js
@@ -29,12 +29,15 @@ $(document).ready(function() {
         slidesToShow: 4,
         slidesToScroll: 4
     });
+	
+	
+	
     $('.responsive').slick({
         dots: true,
         infinite: false,
         speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         responsive: [{
             breakpoint: 1024,
             settings: {
@@ -57,6 +60,9 @@ $(document).ready(function() {
             }
         }]
     });
+
+
+
 
     $('.center').slick({
         centerMode: true,
