fe2fc56ea26ab55d9aacdd5b3a5d42bbdc86c48c
Nate Irwin <nate@nateirwin.net>
2015-03-05 22:12:52 -0700

    Fix Slick error

--- assets/js/global.js
+++ assets/js/global.js
@@ -764,34 +764,36 @@ NPS.newContent = {
 };
 NPS.slick = {
   setup: function() {
-    jQuery('.responsive').slick({
-      dots: true,
-      draggable: false,
-      infinite: false,
-      speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      responsive: [{
-        breakpoint: 992,
-        settings: {
-          dots: true,
-          slidesToScroll: 3,
-          slidesToShow: 3
-        }
-      },{
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },{
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }]
-    });
+    if (jQuery('.responsive').length) {
+      jQuery('.responsive').slick({
+        dots: true,
+        draggable: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [{
+          breakpoint: 992,
+          settings: {
+            dots: true,
+            slidesToScroll: 3,
+            slidesToShow: 3
+          }
+        },{
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },{
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
+      });
+    }
   }
 };
 NPS.starRating = {
